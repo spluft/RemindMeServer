ca91533dbe32d02100cfb33f0327410a97102ac5
Nikolay <spluft@ya.ru>
2018-04-30 16:34:39 +0300

    add to DatabaseConfig getHybernateProperties and container entity manager factory

--- src/main/java/ru/spluft/remindme/server/config/DatabaseConfig.java
+++ src/main/java/ru/spluft/remindme/server/config/DatabaseConfig.java
@@ -7,10 +7,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories("ru.spluft.remindme.server.repository")
@@ -21,6 +26,29 @@ public class DatabaseConfig {
 
     @Resource
     private Environment env;
+
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setPackagesToScan("db.enity.package");
+        entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactory.setJpaProperties(getHybernateProperties());
+
+        return entityManagerFactory;
+    }
+
+    public Properties getHybernateProperties() {
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("hibernate.properties");
+            properties.load(inputStream);
+            return properties;
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Can't find 'hibernate.properties' in classpath", e);
+        }
+    }
 
     @Bean
     public DataSource dataSource() {
