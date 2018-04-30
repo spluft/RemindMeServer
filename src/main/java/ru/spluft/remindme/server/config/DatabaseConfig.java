57de6141f52f379b62c5f85d30f7b41580a927aa
Nikolay <spluft@ya.ru>
2018-04-30 14:40:19 +0300

    add bean datasource

--- src/main/java/ru/spluft/remindme/server/config/DatabaseConfig.java
+++ src/main/java/ru/spluft/remindme/server/config/DatabaseConfig.java
@@ -1,5 +1,7 @@
 package ru.spluft.remindme.server.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -8,6 +10,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories("ru.spluft.remindme.server.repository")
@@ -18,5 +21,15 @@ public class DatabaseConfig {
 
     @Resource
     private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(env.getRequiredProperty("db.url"));
+        basicDataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+        basicDataSource.setUsername(env.getRequiredProperty("db.username"));
+        basicDataSource.setPassword(env.getRequiredProperty("db.password"));
+        return basicDataSource;
+    }
 
 }
