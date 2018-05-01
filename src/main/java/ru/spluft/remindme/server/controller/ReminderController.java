b40aba871e5e4c70737d97bf4f6079a13f13877f
Nikolay <spluft@ya.ru>
2018-05-01 14:17:32 +0300

    edit address for request remind -> reminder return remine instead string in getRemind create mock remind

--- src/main/java/ru/spluft/remindme/server/controller/ReminderController.java
+++ src/main/java/ru/spluft/remindme/server/controller/ReminderController.java
@@ -1,18 +1,29 @@
 package ru.spluft.remindme.server.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.spluft.remindme.server.entity.Remind;
 
-@Controller
-@RequestMapping("/remind")
+import java.util.Date;
+
+@RestController
+@RequestMapping("/reminder")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getRemind(ModelMap model){
-        return "some text";
+    public Remind getRemind(ModelMap model) {
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setRemindDate(new Date());
+        remind.setTitle("First remind");
+        return remind;
     }
 }
